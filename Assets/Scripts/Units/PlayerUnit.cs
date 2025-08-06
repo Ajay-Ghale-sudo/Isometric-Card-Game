ed42e28c258af4655231a23c70ac3b18943e8fb0
andrewkarj <andrewkarj@gmail.com>
2025-08-06 15:10:32 -0700

    PlayerUnit now has transforms to assign to FocusTarget

--- Assets/Scripts/Units/PlayerUnit.cs
+++ Assets/Scripts/Units/PlayerUnit.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 public class PlayerUnit : MonoBehaviour
 {
     [SerializeField] private FocusTarget _focusTarget;
+    [SerializeField] private Transform _camLookTarget;
+    [SerializeField] private Transform _camMoveTarget;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _focusTarget.moveTransform = _camMoveTarget;
+        _focusTarget.focusTransform = _camLookTarget;
     }
 
     // Update is called once per frame
