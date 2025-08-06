8f9c02d1e0a3ae0557b022ede5063dac1af17f21
andrewkarj <andrewkarj@gmail.com>
2025-08-06 14:21:05 -0700

    Integrate camera state machine with focusable target support. Implemented smooth camera movement and rotation.

--- Assets/Scripts/Camera/CameraManager.cs
+++ Assets/Scripts/Camera/CameraManager.cs
@@ -9,11 +9,36 @@ public class CameraManager : MonoBehaviour
     [SerializeField] private Camera _cam;
     [SerializeField] private CinemachineCamera _cinemachineCamera;
     [SerializeField] private Scene _scene;
-    [SerializeField] private ScriptableObject _camTarget;
+    [SerializeField] private FocusTarget _camTarget;
 
-    void Start()
+    private CameraStateMachine _cameraStateMachine;
+    public UnityEvent TryFocusTargetEvent;
+
+    void Awake()
     {
         _cam = Camera.main;
+        print(_cam);
+        _cameraStateMachine = new CameraStateMachine();
+        _cameraStateMachine.ChangeState(new TestState());
+    }
+
+    void Update()
+    {
+        _cameraStateMachine.Tick();
+    }
+
+    public void FocusTarget()
+    {
+        if (_camTarget is FocusTarget focusTarget)
+        {
+            FocusTargetState focusState = new FocusTargetState(_cameraStateMachine, null, _camTarget, _cam);
+            _cameraStateMachine.ChangeState(focusState);
+            TryFocusTargetEvent?.Invoke();
+        }
+        else
+        {
+            Debug.LogError("Camera target is not a FocusTarget ScriptableObject");
+        }
     }
 }
 
@@ -24,14 +49,21 @@ public class CameraStateMachine
 
     private void Start()
     {
-        
     }
 
     public void ChangeState(IState newState)
     {
         _currentState?.Exit();
         _currentState = newState;
-        _currentState?.Enter();
+        if (_currentState != null)
+        {
+            _currentState?.Enter();
+        }
+        else
+        {
+            Debug.LogError("Camera attempted transition to invalid state.");
+            Application.Quit();
+        }
     }
 
     public void Tick()
@@ -46,13 +78,16 @@ public class FocusTargetState : IState
     private GameLoop _game;
     private Camera _camera;
     private Transform _focusTarget;
-    private Transform _transform;
+    private Transform _moveTarget;
     private UnityEvent OnFocusTargetEvent;
 
-    public FocusTargetState(CameraStateMachine cameraStateMachine, GameLoop game)
+    public FocusTargetState(CameraStateMachine cameraStateMachine, GameLoop game, FocusTarget focusTarget, Camera cam)
     {
         _cameraStateMachine = cameraStateMachine;
         _game = game;
+        _focusTarget = focusTarget.focusTransform;
+        _camera = cam;
+        if (focusTarget.moveTransform != null) _moveTarget = focusTarget.moveTransform;
     }
     
     public void Enter()
@@ -63,6 +98,27 @@ public class FocusTargetState : IState
     public void Exit()
     {
         Debug.Log("Camera exited focus target state.");
+    }
+
+    public void Tick()
+    {
+        _camera.transform.position = Vector3.Lerp(_camera.transform.position, _moveTarget.position, Time.deltaTime * 10);
+        Vector3 direction = _focusTarget.position - _moveTarget.position;
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
+        _camera.transform.rotation = Quaternion.Lerp(_camera.transform.rotation, targetRotation, Time.deltaTime * 10);
+    }
+}
+
+public class TestState : IState
+{
+    public void Enter()
+    {
+        Debug.Log("Entering test state.");
+    }
+
+    public void Exit()
+    {
+        Debug.Log("Exiting test state.");
     }
 
     public void Tick()
--- Assets/Scripts/Camera/FocusTarget.cs
+++ Assets/Scripts/Camera/FocusTarget.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "FocusTarget", menuName = "Camera/FocusTarget")]
 public class FocusTarget : ScriptableObject
 {
-    public Transform transform;
-    public GameObject target;
+    [SerializeField] public Transform moveTransform;
+    [SerializeField] public Transform focusTransform;
+    [SerializeField] public GameObject target;
 }
